d92aa5e577ef83d57bbebc2684cc23d09b6b104b
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-08-02 18:38:24 +0530

    feat: Add custom breakpoints and fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,63 @@
 "use client";
 
 import { Link } from "@chakra-ui/next-js";
-import { HStack, IconButton } from "@chakra-ui/react";
+import { Button, HStack, IconButton, Text } from "@chakra-ui/react";
 import { Bookmark, Code, House, Newspaper } from "lucide-react";
 
 export default function Navbar() {
     return (
-        <HStack className="m-auto w-fit border-2 rounded-md border-white p-2 mt-4">
-            <Link href="/">
-                <IconButton
-                    variant="outline"
-                    aria-label="home"
-                    icon={<House color="white" />}
-                />
-            </Link>
-            <IconButton
-                variant="outline"
-                aria-label="articles"
-                icon={<Newspaper color="white" />}
-            />
-            <IconButton
-                variant="outline"
-                aria-label="code"
-                icon={<Code color="white" />}
-            />
-            <Link href="/bookmarks">
-                <IconButton
-                    variant="outline"
-                    aria-label="bookmarks"
-                    icon={<Bookmark color="white" />}
-                />
-            </Link>
-        </HStack>
+        <div className="flex items-center">
+            <HStack className="m-auto w-full border-2 rounded-md border-white p-2 mt-4 justify-center">
+                <Link href="/">
+                    <Button
+                        className="flex items-center space-x-2"
+                        variant={"outline"}
+                    >
+                        <House
+                            className="size-[22px] tablet:size-[18px]"
+                            color="white"
+                        />
+                        <Text className="hidden tablet:block">Home</Text>
+                    </Button>
+                </Link>
+                <Link href="/">
+                    <Button
+                        className="flex items-center space-x-2"
+                        variant={"outline"}
+                    >
+                        <Newspaper
+                            className="size-[22px] tablet:size-[18px]"
+                            color="white"
+                        />
+                        <Text className="hidden tablet:block">Articles</Text>
+                    </Button>
+                </Link>
+                <Link href="/">
+                    <Button
+                        className="flex items-center space-x-2"
+                        variant={"outline"}
+                    >
+                        <Code
+                            className="size-[22px] tablet:size-[18px]"
+                            color="white"
+                        />
+                        <Text className="hidden tablet:block">Tutorials</Text>
+                    </Button>
+                </Link>
+
+                <Link href="/">
+                    <Button
+                        className="flex items-center space-x-2"
+                        variant={"outline"}
+                    >
+                        <Bookmark
+                            className="size-[22px] tablet:size-[18px]"
+                            color="white"
+                        />
+                        <Text className="hidden tablet:block">Bookmarks</Text>
+                    </Button>
+                </Link>
+            </HStack>
+        </div>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,14 @@ const config = {
     ],
     prefix: "",
     theme: {
+        screens: {
+            mobileS: "320px",
+            mobileM: "375px",
+            mobileL: "425px",
+            tablet: "768px",
+            laptop: "1024px",
+            laptopL: "1440px",
+        },
         extend: {
             colors: {
                 graymodern: {
