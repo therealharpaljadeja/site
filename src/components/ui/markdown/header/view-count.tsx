81d2f0b52dcb32fa54727bc1866b6e144344f797
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-09-06 15:34:36 +0530

    fix: view api url

--- src/components/ui/markdown/header/view-count.tsx
+++ src/components/ui/markdown/header/view-count.tsx
@@ -5,7 +5,7 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function BlogViewCount({ id }: { id: string }) {
-    const { data } = useSWR(`/api/views?id=${id}&incr=1`, fetcher);
+    const { data } = useSWR(`/api/blog/views?id=${id}&incr=1`, fetcher);
 
     return (
         <span className="text-graymodern-500 font-body">
